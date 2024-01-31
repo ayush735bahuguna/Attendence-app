c20d5516475e825d392f972d9542fd5397f3695b
Ayush bahuguna <ayushbahuguna1122@gmail.com>
2024-01-31 19:50:49 +0000

    bottom tab added

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
 
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import Home from './Screens/Home/Home'
+import Settings from './Screens/Settings/Settings'
+
+const Tab = createMaterialBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+
+        <Tab.Navigator
+          initialRouteName="Home"
+        // activeColor="#f0edf6"
+        // inactiveColor="#3e2465"
+        // barStyle={{ backgroundColor: '#694fad' }}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
